3f2e06f24757f116ed06f77817d6eb5cadc83814
Peter Kolloch <info@eigenvalue.net>
2019-04-21 08:30:15 +0200

    Pin nixos version used in shell.nix

--- shell.nix
+++ shell.nix
@@ -2,7 +2,14 @@
 # Crates a shell with crate2nix installed.
 #
 
-let nixpkgs = builtins.fetchGit { url = https://github.com/NixOS/nixpkgs-channels.git; ref = "nixos-unstable"; };
+# Use pinned version of nixos-unstable by default to make sure this works.
+# Override with e.g.
+#    nix-shell --arg pkgs 'import <nixos> {config = {}; }'
+let nixpkgs = builtins.fetchTarball {
+    name = "nixos-unstable-2019-04-21";
+    url = https://github.com/nixos/nixpkgs/archive/1fc591f9a5bd1b016b5d66dfab29560073955a14.tar.gz;
+    sha256 = "1ij5x1qw486rbih7xh2c01s60c3zblj6ad1isf5y99sh47jcq76c";
+};
 
 in
 
