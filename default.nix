bb8c499e9865616d81d263c6d81e05d2f47267d4
Peter Kolloch <info@eigenvalue.net>
2019-04-28 19:36:16 +0200

    Fix indention of default.nix

--- default.nix
+++ default.nix
@@ -9,18 +9,18 @@ let cargo_nix = callPackage ./Cargo.nix {};
     crate2nix = cargo_nix.root_crate;
 
 in pkgs.symlinkJoin {
-      name = crate2nix.name;
-      paths = [ crate2nix ];
-      buildInputs = [ makeWrapper cargo ];
-      postBuild = ''
-          # Fallback to built dependencies for cargo and nix-prefetch-url
-          wrapProgram $out/bin/crate2nix \
-             --suffix PATH ":" ${lib.makeBinPath [ cargo nix ]}
-          rm -rf $out/lib $out/bin/crate2nix.d
-          mkdir -p \
-            $out/share/bash-completion/completions \
-            $out/share/zsh/vendor-completions
-          $out/bin/crate2nix completions -s 'bash' -o $out/share/bash-completion/completions
-          $out/bin/crate2nix completions -s 'zsh' -o $out/share/zsh/vendor-completions
-      '';
-    }
+  name = crate2nix.name;
+  paths = [ crate2nix ];
+  buildInputs = [ makeWrapper cargo ];
+  postBuild = ''
+    # Fallback to built dependencies for cargo and nix-prefetch-url
+    wrapProgram $out/bin/crate2nix \
+       --suffix PATH ":" ${lib.makeBinPath [ cargo nix ]}
+    rm -rf $out/lib $out/bin/crate2nix.d
+    mkdir -p \
+      $out/share/bash-completion/completions \
+      $out/share/zsh/vendor-completions
+    $out/bin/crate2nix completions -s 'bash' -o $out/share/bash-completion/completions
+    $out/bin/crate2nix completions -s 'zsh' -o $out/share/zsh/vendor-completions
+  '';
+}
