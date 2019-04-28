4c25d2d571f5465d2f7f2f333c6508ad183e1403
Peter Kolloch <info@eigenvalue.net>
2019-04-28 12:31:45 +0200

    Fix #6 - Update nixpkgs to a recent version including NixOS/nixpkgs#60127

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,7 +1,7 @@
 let nixpkgs = builtins.fetchTarball {
-    name = "nixos-unstable-2019-04-22";
-    url = https://github.com/nixos/nixpkgs/archive/d26027792812fbfad4d0f451b5f47fdabf7fdeb9.tar.gz;
-    sha256 = "10agvpj4d62yvwxwflgxbg1dxp654r4cwirpbsrmfikp0wsvijsv";
+    name = "nixos-unstable-2019-04-28";
+    url = https://github.com/nixos/nixpkgs/archive/a2e219ec879fe3de62e9204ad33efb5b67a4a324.tar.gz;
+    sha256 = "0bdr8ii6kc1rd4l5dnmqpb5ljh2wk47y75n9ibg2ddc62hi6jxq9";
 };
 
 in import nixpkgs
