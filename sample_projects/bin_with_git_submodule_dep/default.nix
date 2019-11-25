9db9b413fa6e770f4328df2186758521b47c0010
Phillip Cloud <cloud@standard.ai>
2019-11-25 11:16:09 -0500

    Need more build inputs

--- sample_projects/bin_with_git_submodule_dep/default.nix
+++ sample_projects/bin_with_git_submodule_dep/default.nix
@@ -2,8 +2,11 @@
 
 let customBuildRustCrate = pkgs.buildRustCrate.override {
   defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-    librocksdb-sys = attrs: {
+    librocksdb-sys = attrs: with pkgs; {
       src = attrs.src + "/librocksdb-sys";
+      buildInputs = [ clang rocksdb ];
+      LIBCLANG_PATH="${clang.cc.lib}/lib";
+      ROCKSDB_LIB_DIR = "${rocksdb}/lib/";
     };
   };
 };
