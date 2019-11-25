4b5fdd698ea24e837f3dece5523ab218e5fcc2d7
Phillip Cloud <cloud@standard.ai>
2019-11-24 23:25:59 -0500

    Fix default

--- sample_projects/bin_with_git_submodule_dep/default.nix
+++ sample_projects/bin_with_git_submodule_dep/default.nix
@@ -1,9 +1,10 @@
 { pkgs? import <nixpkgs> { config = {}; } }:
+
 let basePackage = pkgs.callPackage ./Cargo.nix {};
-    submoduleCargoNix = basePackage.rootCrate.build.override {
+    submodulePackage = basePackage.rootCrate.build.override {
       crateOverrides = {
         librocksdb-sys = attrs: {
           src = attrs.src + "/librocksdb-sys";
         };
       };
-}; in submoduleCargoNix
+    }; in submodulePackage
