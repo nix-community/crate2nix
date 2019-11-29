52a2684709fee197338a06dd3771553a54008919
Peter Kolloch <info@eigenvalue.net>
2019-11-29 09:04:03 +0100

    Add cache autopush to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,3 +5,12 @@ top="$(readlink -f "$(dirname "$0")")"
 cd "$top"/crate2nix
 
 ../regenerate_cargo_nix.sh && nix run nixpkgs.cargo -c cargo test
+
+# Crude hack check if we have the right to push to the cache
+if grep -q '"eigenvalue"' ~/.config/cachix/cachix.dhall; then
+    # we filter for "rust_" to exclude some things that are in the
+    # nixos cache anyways
+    nix-store -q -R --include-outputs $(nix-store -q -d result*) |\
+     grep -e "-rust_" |\
+     cachix push eigenvalue
+fi
