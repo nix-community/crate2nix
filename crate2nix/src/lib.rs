0ac6acd5deac9d9be4a95f2e3eebf15d3041b740
Phillip Cloud <cloud@standard.ai>
2020-01-27 12:19:05 -0800

    Add back eprintln

--- crate2nix/src/lib.rs
+++ crate2nix/src/lib.rs
@@ -152,6 +152,11 @@ fn prefetch_and_fill_crates_sha256(
     }) {
         if let Some(hash) = lock_file.get_hash(&package.package_id.repr)? {
             package.source = package.source.with_sha256(hash);
+        } else {
+            eprintln!(
+                "Lock file incomplete, hash for {} missing.",
+                package.package_id
+            );
         }
     }
 
