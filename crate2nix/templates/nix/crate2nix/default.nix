dd34971fd8d99fe761c09aa1c5c659a3f874bdb8
Symphorien Gibol <symphorien+git@xlumurb.eu>
2020-04-08 12:00:00 +0000

    use lib.concatMap instead of builtins.concatMap
    travis has nix 2.0.4 which does not have the latter.

--- crate2nix/templates/nix/crate2nix/default.nix
+++ crate2nix/templates/nix/crate2nix/default.nix
@@ -512,7 +512,7 @@ rec {
       expandFeature = feature:
         assert (builtins.isString feature);
         [ feature ] ++ (expandFeatures featureMap (featureMap."${feature}" or []));
-      outFeatures = builtins.concatMap expandFeature inputFeatures;
+      outFeatures = lib.concatMap expandFeature inputFeatures;
     in
       sortedUnique outFeatures;
 
