70d2805842e6fdb545e10ee984809dc08fd61103
Cole Helbling <cole.e.helbling@outlook.com>
2021-02-25 20:56:20 -0800

    treewide: stdenv.lib -> lib
    stdenv.lib is a deprecated alias to lib.

--- sample_projects/codegen/Cargo.nix
+++ sample_projects/codegen/Cargo.nix
@@ -791,7 +791,7 @@ rec {
                       crateConfig.sha256;
                   }
                 );
-                extraRustcOpts = lib.lists.optional (targetFeatures != [ ]) "-C target-feature=${stdenv.lib.concatMapStringsSep "," (x: "+${x}") targetFeatures}";
+                extraRustcOpts = lib.lists.optional (targetFeatures != [ ]) "-C target-feature=${lib.concatMapStringsSep "," (x: "+${x}") targetFeatures}";
                 inherit features dependencies buildDependencies crateRenames release;
               }
             );
