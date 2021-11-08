c7d8d3fc63e5d86ec8fd0524fc26ab45f3b06643
Alexander Bantyev <balsoft@balsoft.ru>
2021-11-08 10:12:19 +0300

    fixup! Fix broken IFD build with Nix 2.4

--- sample_projects/bin_with_git_submodule_dep/Cargo.nix
+++ sample_projects/bin_with_git_submodule_dep/Cargo.nix
@@ -226,7 +226,7 @@ rec {
         ];
         # We can't filter paths with references in Nix 2.4
         # See https://github.com/NixOS/nix/issues/5410
-        src = if (lib.versionOlder builtins.nixVersion "2.4")
+        src = if (lib.versionOlder "2.4" builtins.nixVersion)
           then lib.cleanSourceWith { filter = sourceFilter;  src = ./.; }
           else ./.;
         authors = [
