22c7b223493d133e74841013e556209acebce773
Bouke van der Bijl <i@bou.ke>
2023-09-18 16:12:13 +0200

    Run nixpkgs-fmt
    Also add formatter to flake so you can do `nix fmt`

--- flake.nix
+++ flake.nix
@@ -7,13 +7,16 @@
     overlays.default = self: super: {
       crate2nix = self.callPackage ./default.nix { };
     };
-  } // flake-utils.lib.eachDefaultSystem (system: let
-    sources = import ./nix/sources.nix;
-    pkgs = import sources.nixpkgs { inherit system; };
-  in {
-    packages = rec {
-      crate2nix = pkgs.callPackage ./default.nix { };
-      default = crate2nix;
-    };
-  });
+  } // flake-utils.lib.eachDefaultSystem (system:
+    let
+      sources = import ./nix/sources.nix;
+      pkgs = import sources.nixpkgs { inherit system; };
+    in
+    {
+      formatter = pkgs.nixpkgs-fmt;
+      packages = rec {
+        crate2nix = pkgs.callPackage ./default.nix { };
+        default = crate2nix;
+      };
+    });
 }
