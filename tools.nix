510373d73de0b009efef40e5ca838fb157902294
tilpner <git@tilpner.com>
2023-09-19 11:24:01 +0200

    feat(tools): resolve workspace-inherited fields in manifests
    When copying out workspace member crates from a larger workspace,
    cargo will not be able to process the separated manifest if it still
    refers to e.g. the workspace license or workspace dependencies, because
    it can't find the workspace manifest.
    
    We can't help it find the workspace manifest, because crates in a cargo
    vendor directory aren't discovered recursively, and cargo doesn't resolve
    symlinks before checking the ancestors to find the workspace manifest.

--- tools.nix
+++ tools.nix
@@ -432,6 +432,11 @@ rec {
               ''
                 mkdir -p $out
                 cp -apR ${src}/${pathToExtract}/* $out
+                cd $out
+
+                mv ./Cargo.toml ./Cargo.toml.orig
+                ${crate2nix}/bin/crate2nix resolve-manifest --cargo-toml ${src}/${pathToExtract}/Cargo.toml > ./Cargo.toml
+
                 echo '{"package":null,"files":{}}' > $out/.cargo-checksum.json
               '';
         };
