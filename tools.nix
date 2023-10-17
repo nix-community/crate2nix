929e4b937ced68c8da7d2bf2819406fc0ab242d7
Peter Kolloch <info@eigenvalue.net>
2023-10-17 07:48:45 +0200

    [tools.nix] reformat

--- tools.nix
+++ tools.nix
@@ -407,19 +407,24 @@ rec {
               rootCargo = builtins.fromTOML (builtins.readFile "${src}/Cargo.toml");
               isWorkspace = rootCargo ? "workspace";
               isPackage = rootCargo ? "package";
-              containedCrates = rootCargo.workspace.members ++ (if isPackage then ["."] else []);
+              containedCrates = rootCargo.workspace.members ++ (if isPackage then [ "." ] else [ ]);
 
-              getCrateNameFromPath = path: let
-                cargoTomlCrate = builtins.fromTOML (builtins.readFile "${src}/${path}/Cargo.toml");
-              in
+              getCrateNameFromPath = path:
+                let
+                  cargoTomlCrate = builtins.fromTOML (builtins.readFile "${src}/${path}/Cargo.toml");
+                in
                 cargoTomlCrate.package.name;
 
-              pathToExtract = if isWorkspace then
-                builtins.head (builtins.filter (to_filter:
-                  (getCrateNameFromPath to_filter) == name
-                ) containedCrates)
-              else
-               ".";
+              pathToExtract =
+                if isWorkspace then
+                  builtins.head
+                    (builtins.filter
+                      (to_filter:
+                        (getCrateNameFromPath to_filter) == name
+                      )
+                      containedCrates)
+                else
+                  ".";
             in
             pkgs.runCommand (lib.removeSuffix ".tar.gz" src.name) { }
               ''
