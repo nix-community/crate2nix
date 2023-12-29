c9be9d67f8c7564a07d4aa0ffcb9d57a3af5e79c
Peter Kolloch <info@eigenvalue.net>
2023-12-29 13:25:19 +0100

    [flake] using flake-parts

--- flake.nix
+++ flake.nix
@@ -1,26 +1,30 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+  description = "crate2nix generates nix build files for rust crates using cargo";
+
+  inputs = {  
+
   };
 
-  outputs = { self, flake-utils }: {
-    overlays.default = final: prev: {
-      crate2nix = self.callPackage ./default.nix { };
+  outputs = inputs@{ nixpkgs, self, flake-parts }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+    flake = {
+      overlays.default = final: prev: {
+        crate2nix = self.callPackage ./default.nix { };
+      };
+      templates.default = {
+        path = ./template;
+        description = "An example of crate2nix";
+      };
     };
-    templates.default = {
-      path = ./template;
-      description = "An example of crate2nix";
-    };
-  } // flake-utils.lib.eachDefaultSystem (system:
-    let
-      sources = import ./nix/sources.nix;
-      pkgs = import sources.nixpkgs { inherit system; };
-    in
-    {
+
+    perSystem = { config, self', inputs', pkgs, system, ... }: {
       formatter = pkgs.nixpkgs-fmt;
+      checks = config.packages;
       packages = rec {
         crate2nix = pkgs.callPackage ./default.nix { };
         default = crate2nix;
       };
-    });
+    };
+  };
 }
