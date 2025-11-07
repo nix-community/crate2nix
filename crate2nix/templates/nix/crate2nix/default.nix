b37ab852712671bce8158c830f13ce689edb3755
Josh Robson Chase <josh@robsonchase.com>
2025-11-06 21:31:34 -0500

    inherit the main crate's `meta` when building with tests

--- crate2nix/templates/nix/crate2nix/default.nix
+++ crate2nix/templates/nix/crate2nix/default.nix
@@ -205,7 +205,7 @@ rec {
       in
       pkgs.runCommand "${crate.name}-linked"
         {
-          inherit (crate) outputs crateName;
+          inherit (crate) outputs crateName meta;
           passthru = (crate.passthru or { }) // {
             inherit test;
           };
